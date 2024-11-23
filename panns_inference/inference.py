e944a756d2c3d69f8a574432d7179f5937b2559a
Ryan Johnson <ryzn.johnson@gmail.com>
2024-11-22 22:09:31 -0500

    allow for mps directly

--- panns_inference/inference.py
+++ panns_inference/inference.py
@@ -38,7 +38,7 @@ class AudioTagging(object):
 
         if device == 'cuda' and torch.cuda.is_available():
             self.device = 'cuda'
-        elif device == 'cuda' and not torch.cuda.is_available() and torch.backends.mps.is_available():
+        elif device == 'cuda' and not torch.cuda.is_available() and torch.backends.mps.is_available() or (device == 'mps' and torch.backends.mps.is_available()):
             self.device = 'mps'
         else:
             self.device = 'cpu'
